2d7f2eb10b490478156ea2b08fb1ac8de93248ce
Diogo Pinto <diogocp27@hotmail,com>
2023-10-25 09:35:50 +0100

    added Lanterna to the dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,9 @@ repositories {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/com.googlecode.lanterna/lanterna
+    implementation group: 'com.googlecode.lanterna', name: 'lanterna', version: '3.1.1'
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
